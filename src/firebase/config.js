d4f76f010eac3a89922d6e8ef18abf61affa35ee
Rossano-DAngelo <rossano.dangelo@ibm.com>
2020-01-08 15:34:33 +0000

    upload initial code

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+const devConfig = {
+  apiKey: "AIzaSyAF853940HQ_BZiJmEl6Y2S3w7aA9SpEdc",
+  authDomain: "react-firebase-authentic-f6d18.firebaseapp.com",
+  databaseURL: "https://react-firebase-authentic-f6d18.firebaseio.com",
+  projectId: "react-firebase-authentic-f6d18",
+  storageBucket: "react-firebase-authentic-f6d18.appspot.com",
+  messagingSenderId: "178061036283",
+  appId: "1:178061036283:web:594669aaf4ae92b7677e5e"
+};
+
+const prodConfig = {
+  apiKey: "AIzaSyAF853940HQ_BZiJmEl6Y2S3w7aA9SpEdc",
+  authDomain: "react-firebase-authentic-f6d18.firebaseapp.com",
+  databaseURL: "https://react-firebase-authentic-f6d18.firebaseio.com",
+  projectId: "react-firebase-authentic-f6d18",
+  storageBucket: "react-firebase-authentic-f6d18.appspot.com",
+  messagingSenderId: "178061036283",
+  appId: "1:178061036283:web:594669aaf4ae92b7677e5e"
+};
+
+export {
+  devConfig,
+  prodConfig
+}
